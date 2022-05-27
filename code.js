c151724d00bc532678ab19fca99b18d7b6c54c1b
03Aniar <aniar.kaldybaiuly@nu.edu.kz>
2022-05-27 19:41:37 +0600

    write the code for the game, but cannot resolve it

---
+++ code.js
@@ -0,0 +1,37 @@
+let playerSelection = "rock";
+let computerSelection = computerPlay();
+console.log(single_round(playerSelection, computerSelection));
+
+function computerPlay() {
+    let x = Math.floor(Math.random() * (4 - 1) + 1);
+    switch (x) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+
+    }
+}
+
+function single_round(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) return "You have a draw";
+
+    switch (playerSelection.conCat("", computerSelection)) {
+
+        case "rockpaper": case "scissorsrock": case "paperscissors":
+            return "You Lost!";
+
+        case "paperrock": case "rockscissors": case "scissorspaper":
+            return "You Won!";
+
+        default:
+            return "OOPs, something went wrong!!!"
+    }
+}
